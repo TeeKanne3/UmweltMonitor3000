9833dc6608583eec1e30dcd1779ad69cd2b6f33b
nico.koch <nico.koch@R3208-PC27>
2026-03-11 14:06:54 +0100

    Add Folder Structure

---
+++ Application/Repositories/UmweltMonitorRepository.cs
@@ -0,0 +1,15 @@
+﻿using UmweltMonitor3000.Application.Interfaces;
+
+namespace UmweltMonitor3000.Application.Repositories;
+
+public class UmweltMonitorRepository : IUmweltMonitorRepository
+{
+    public Task SaveSensorDataAsync(string sensorId, string data)
+    {
+        throw new NotImplementedException();
+    }
+    public Task<string> GetSensorDataAsync(string sensorId)
+    {
+        throw new NotImplementedException();
+    }
+}
