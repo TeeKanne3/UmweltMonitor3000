0ece789fa8aa7db1cb9fa5e8738b6bbe20b0ec63
nico.koch <nico.koch@R3208-PC27>
2026-03-11 14:28:42 +0100

    Schneller Bug Fix, um die Solution wieder zu builden.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,13 @@
 ﻿using System.Configuration;
 using System.Data;
-using System.Windows;
+using ApplicationFromWindows = System.Windows.Application;
 
 namespace UmweltMonitor3000
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : ApplicationFromWindows
     {
     }
 
