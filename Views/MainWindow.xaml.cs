617170a89e0d85d9f63219be496ebade35417533
max.wolf <max.wolf@R3208-PC26>
2026-03-18 14:37:29 +0100

    changes

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using UmweltMonitor3000.Application.Models;
+using UmweltMonitor3000.Application.ViewModels;
 using UmweltMonitor3000.Application.Services;
 using UmweltMonitor3000.Application.ViewModels;
 
@@ -27,26 +28,7 @@ namespace UmweltMonitor3000
         {
             InitializeComponent();
             Loaded += (_, _) => DarkTitleBar.TrySetDarkTitleBar(this);
-
-            _logic.LogMessage += msg =>
-            {
-                Dispatcher.Invoke(() =>
-                {
-                    // Maybe we have the option here, to Write the Data to the DataGrid.
-
-                    Console.WriteLine(msg);
-                });
-            };
-        }
-
-        private async void Connect_Click(object sender, RoutedEventArgs e)
-        {
-            await _logic.Connect("127.0.0.1", 1883);
-        }
-
-        private async void Disconnect_Click(object sender, RoutedEventArgs e)
-        {
-            await _logic.Disconnect();
+            DataContext = new MainViewModel();
         }
     }
 }
