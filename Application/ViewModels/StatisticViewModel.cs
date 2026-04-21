959d651c343ea2c356c650f139a62a37dfbb482a
Wolf, Max <max.wolf@kodak.com>
2026-04-21 15:35:19 +0200

    UI changes

--- Application/ViewModels/StatisticViewModel.cs
+++ Application/ViewModels/StatisticViewModel.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Mvvm.Input;
 using LiveChartsCore;
 using LiveChartsCore.Defaults;
+using LiveChartsCore.Drawing;
 using LiveChartsCore.Kernel;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
@@ -17,6 +18,9 @@ namespace UmweltMonitor3000.Application.ViewModels;
 
 public partial class StatisticViewModel : ObservableObject
 {
+    [ObservableProperty]
+    public partial double MoisturePercent {  get; set; }
+
     //Linien Chart Properties
     [ObservableProperty]
     public partial ISeries[] PlantSeries { get; set; }
@@ -41,48 +45,67 @@ public partial class StatisticViewModel : ObservableObject
         UnitWidth = 1,
     }
 };
-    public Axis[] PlantSeriesYAxes { get; set; } = {
+    public Axis[] PlantSeriesYAxes { get; set; } = [
     new Axis
     {
-       MinLimit = 0,
+        MinLimit = 0,
         MaxLimit = 100,
         LabelsPaint = new SolidColorPaint(SKColors.White),
-        Labeler = value => $"{value}%", // Fügt ein Prozentzeichen hinzu
+        Labeler = value => $"{value}%", 
         SeparatorsPaint = new SolidColorPaint(SKColors.White.WithAlpha(30)) { StrokeThickness = 1 }
-    }
-};
-    public RectangularSection[] PlantSeriesSections { get; set; } = {
+    }];
+    public RectangularSection[] PlantSeriesSections { get; set; } = [
     new RectangularSection
     {
         Yi = 0,
         Yj = 20,
         Fill = new SolidColorPaint(SKColors.Red.WithAlpha(50))
-    }
-};
+    }];
 
     //Balkendiagramm für die Feuchtigkeit aller Pflanzen
     [ObservableProperty]
     public partial ISeries[] AllPlantMoisture { get; set; } = [
-        new RowSeries<double> { Values = [920],Fill = new SolidColorPaint(SKColors.DeepSkyBlue),Name = "Lowenzahn"}, 
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
-        new RowSeries<double> { Values = [1000], Fill = new SolidColorPaint(SKColors.Teal), Name = "Hamilton" },
+        new RowSeries<double> { Values = [60],Fill = new SolidColorPaint(SKColors.DeepSkyBlue)},
+        new RowSeries<double> { Values = [70], Fill = new SolidColorPaint(SKColors.Teal)},
+        new RowSeries<double> { Values = [55], Fill = new SolidColorPaint(SKColors.Teal)},
+        new RowSeries<double> { Values = [87], Fill = new SolidColorPaint(SKColors.Teal) },
+        new RowSeries<double> { Values = [39], Fill = new SolidColorPaint(SKColors.Teal) },
+        new RowSeries<double> { Values = [21], Fill = new SolidColorPaint(SKColors.Teal) },
+        new RowSeries<double> { Values = [44], Fill = new SolidColorPaint(SKColors.Teal) },
+        new RowSeries<double> { Values = [61], Fill = new SolidColorPaint(SKColors.Teal) },
+        new RowSeries<double> { Values = [5], Fill = new SolidColorPaint(SKColors.Teal) },
     ];
     public Axis[] AllPlantSeriesXAxes { get; set; } = [
     new Axis
     {
-        LabelsPaint = new SolidColorPaint(SKColors.White), 
-        SeparatorsPaint = new SolidColorPaint(SKColors.Gray.WithAlpha(50)) 
+        
+        LabelsPaint = new SolidColorPaint(SKColors.White),
+        SeparatorsPaint = null,
+        MaxLimit = 100,
+        MinLimit = 0,
+        Labeler = value => $"{value}%",
     }];
     public Axis[] AllPlantSeriesYAxes { get; set; } = [
     new Axis
     {
-        LabelsPaint = new SolidColorPaint(SKColors.White)
+        LabelsPaint = new SolidColorPaint(SKColors.White),
+        Labels =
+        [
+            "Löwenzahn",
+            "Hamilton 1",
+            "Hamilton 2",
+            "Hamilton 3",
+            "Hamilton 4",
+            "Hamilton 5",
+            "Hamilton 6",
+            "Hamilton 7",
+            "Hamilton 8"
+        ],
+
+        TextSize = 14,
+        Padding = new Padding(0, 0, 15, 0),
+        ForceStepToMin = true,
+        MinStep = 1
     }];
 
 
@@ -110,61 +133,15 @@ public partial class StatisticViewModel : ObservableObject
         new PieSeries<double>
         {
             Values = [30.0],
+            Fill = new SolidColorPaint(SKColors.YellowGreen),
             DataLabelsSize = 30,
             MaxRadialColumnWidth = 50,
             DataLabelsPaint = null
         }
             ];
     [ObservableProperty]
-    public partial string MoistureTemperature { get; set; } = "30°C";
+    public partial string MoistureTemperature { get; set; } = "30%";
 
-
-
-    [RelayCommand]
-    public void OnPointMeasured(ChartPoint point)
-    {
-        var plant = (PlantMoisture?)point.Context.DataSource;
-        if (point.Context.Visual is null || plant is null)
-            return;
-
-        point.Context.Visual.Fill = plant.Paint;
-    }
-    private static string GetPlantName(ChartPoint point)
-    {
-        var plant = (PlantMoisture?)point.Context.DataSource;
-        return plant is null ? string.Empty : plant.Name;
-    }
-    private static PlantMoisture[] Fetch()
-    {
-        var paints = Enumerable.Range(0, 7)
-            .Select(i => new SolidColorPaint(ColorPalletes.MaterialDesign500[i].AsSKColor()))
-            .ToArray();
-
-        return [
-            new("Tsunoda",   500,  paints[0]),
-            new("Sainz",     450,  paints[1]),
-            new("Riccardo",  520,  paints[2]),
-            new("Bottas",    550,  paints[3]),
-            new("Perez",     660,  paints[4]),
-            new("Verstapen", 920,  paints[5]),
-            new("Hamilton",  1000, paints[6])
-        ];
-    }
-
-
-
-    public class PlantMoisture : ObservableValue
-    {
-        public PlantMoisture(string name, int value, SolidColorPaint paint)
-        {
-            Name = name;
-            Paint = paint;
-            Value = value;
-        }
-
-        public string Name { get; set; }
-        public SolidColorPaint Paint { get; set; }
-    }
 }
 
 
---
+++ Views/Converter/ValueToRangeConverter2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace UmweltMonitor3000.Views.Converter;
+
+public class ValueToRangeConverter2 : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not int valueToConvert)
+            return "";
+
+        if (valueToConvert <= 30)
+            return "kritisch";
+
+        if (valueToConvert <= 40)
+            return "durstig";
+
+        if (valueToConvert <= 70)
+            return "zunass";
+
+        return "optimal";
+
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return System.Windows.Data.Binding.DoNothing;
+    }
+}
